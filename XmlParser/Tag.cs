cf62168d35dd4b4f7e448c3a3cb5d219419bd490
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2019-08-21 20:35:29 +0300

    Refactoring

--- XmlParser/Tag.cs
+++ XmlParser/Tag.cs
@@ -34,71 +34,78 @@ namespace XmlParser
             var processedTags = new Stack<Tag>();
             while (text.Length > 0)
             {
-                var tagRegexp = new Regex(@"^\<\s*(\w+)(\s+.*)?\>");
+                // Область текста, котору обрабатываем
+                string textScope;
+
+                const string tagPattern = @"^\<(\s*|\/)?(\w[\w\W]+?)(\s.*?)?\>";
+                // 1 группа - закрывающая черта
+                const int FULL_TEXT_GROUP = 0;
+                // 2 группа - название тега
+                const int CLOSING_SLASH_GROUP = 1;
+                // 3 группа - атрибуты
+                const int TAG_NAME_GROUP = 2;
+
+                var tagRegexp = new Regex(tagPattern);
 
                 var tagMatch = tagRegexp.Match(text);
 
                 if (tagMatch.Success)
                 {
-                    var tagName = tagMatch.Groups[1].Value;
+                    // В начале стоит тег
 
-                    var tag = new Tag() { Name = tagName };
-
-                    //TODO: Тут возможна перезапись - нужно реализовывать объект
-                    if (processedTags.Count>0)
+                    var tagName = tagMatch.Groups[TAG_NAME_GROUP].Value;
+                    textScope = tagMatch.Groups[FULL_TEXT_GROUP].Value;
+                    var closingTag = !string.IsNullOrWhiteSpace(tagMatch.Groups[CLOSING_SLASH_GROUP].Value);
+                    if (closingTag)
                     {
                         var lastTag = processedTags.Peek();
-                        lastTag.Subcontent = tag;
-                    }
-                    processedTags.Push(tag);
-                    text = text.Substring(tagMatch.Groups[0].Value.Length);
-                }
-                else
-                {
-
-                    var closedTagRegexp = new Regex(@"^\<\/([\w\W]*?)\>");
-
-                    //Если true, то в начале закрывающий тег и надо достать его из стека
-                    //иначе в начале текст и его надо занести в последний тег
-                    if (closedTagRegexp.IsMatch(text))
-                    {
-                        // Скопировано выше = оптимизировать
-
-                        var tagNameMatch = closedTagRegexp.Match(text);
-
-                        if (tagNameMatch.Success)
+                        if (lastTag.Name == tagName)
                         {
-                            var tagName = tagNameMatch.Groups[1].Value;
-                            var lastTag = processedTags.Peek();
-                            if (lastTag.Name == tagName)
-                            {
-                                processedTags.Pop();
-                                text = text.Substring(tagNameMatch.Groups[0].Value.Length);
-                            }
-                            else
-                            {
-                                throw new Exception("Неправильная структкра XML");
-                            }
+                            processedTags.Pop();
+
+                            //TODO: заносить в структуру документа
+                        }
+                        else
+                        {
+                            throw new Exception("Неправильная структкра XML");
                         }
                     }
                     else
                     {
-                        const string dataPattern = @"^(.*?)\<\/";
-                        var dataExtractRegexp = new Regex(dataPattern);
-                        var dataMatch = dataExtractRegexp.Match(text);
-                        var lastTag = processedTags.Peek();
+                        //TODO: Обработка атрибутов
 
-                        var data = dataMatch.Groups[1].Value;
-                        lastTag.Data = data;
-                        text = text.Substring(data.Length);
+                        var tag = new Tag() { Name = tagName };
+
+                        //TODO: Тут возможна перезапись - нужно реализовывать объект
+                        if (processedTags.Count > 0)
+                        {
+                            var lastTag = processedTags.Peek();
+                            lastTag.Subcontent = tag;
+                        }
+
+                        processedTags.Push(tag);
                     }
-
+                }
+                else
+                {
+                    // В начале стоит контент
+                    var data = ExtractData(text);
+                    var lastTag = processedTags.Peek();
+                    lastTag.Data = data;
+                    textScope = data;
                 }
 
-
+                text = text.Substring(textScope.Length);
             }
+        }
 
-            //Находим название первого тега
+        private static string ExtractData(string text)
+        {
+            const string dataPattern = @"^(.*?)\<\/";
+            var dataExtractRegexp = new Regex(dataPattern);
+            var dataMatch = dataExtractRegexp.Match(text);
+            var data = dataMatch.Groups[1].Value;
+            return data;
         }
 
         /// <summary>
